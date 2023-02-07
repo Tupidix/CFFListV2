3cd4e652cc487ce49813e46f9628c9de036bee14
Ryan Dorasamy <ryan.dorasamy@heig-vd.ch>
2023-02-07 10:44:32 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const getProchainDeparts = async () => {
+    const res = await fetch("http://transport.opendata.ch/v1/stationboard?station=Yverdon-les-Bains&limit=10");
+    const data = await res.json();
+    console.log(data);
+    data.stationboard.forEach(element => {
+        creeArticle(element);
+     });
+}
+
+getProchainDeparts();
+
+const board = document.querySelector("#board");
+
+const creeArticle = (numero) =>{
+    const html = `<article>
+    <div class="time">${numero.stop.departure}</div>
+    <div class="category" data-category="${numero.category}">${numero.category}</div>
+    <div class="destination">${numero.to}</div>
+    </article>`;
+    board.insertAdjacentHTML("beforebegin", html);
+}
